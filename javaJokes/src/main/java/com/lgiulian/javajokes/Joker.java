df4b4df273f90c71ce73b839872f930f1bd2d3c0
iulian <iuliancl2000@yahoo.com>
2018-04-23 13:42:00 +0300

    follow recommendations after second review

--- javaJokes/src/main/java/com/lgiulian/javajokes/Joker.java
+++ javaJokes/src/main/java/com/lgiulian/javajokes/Joker.java
@@ -1,6 +1,6 @@
 package com.lgiulian.javajokes;
 
-public class Joker {
+public final class Joker {
 
     public String getJoke() {
         return "I was going to make myself a belt made out of watches, but then I realized it would be a waist of time.";
